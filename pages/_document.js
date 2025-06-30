c55d4a3128caf196fdeb46f462de7f2cdc5ba6d2
PushkarSAshtekar <pushkar.ashtekar77@gmail.com>
2025-06-30 14:04:17 +0530

    Removed _document.js and fixed Next.js build issues

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,20 @@
-// pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+// // pages/_document.js
+// import { Html, Head, Main, NextScript } from 'next/document';
 
-import Document from 'next/document';
+// import Document from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+// class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
 
-export default MyDocument;
+// export default MyDocument;
