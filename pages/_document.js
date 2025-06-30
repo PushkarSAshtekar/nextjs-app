2402a80bb991a0d0b61dec6fb45acf78cfa2204b
PushkarSAshtekar <pushkar.ashtekar77@gmail.com>
2025-06-30 13:43:43 +0530

    ✅ Successful build: Fixed _document.js error

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,23 @@
 // pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
   render() {
     return (
-      <>
+      <Html>
         <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </>
-    );
+      </Html>
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
